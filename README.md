f2433c954fbf60e91e43550a1e95e2089e11b005
David Burt <burt.david.j@gmail.com>
2024-02-13 16:58:11 +1100

    checkworks

---
+++ README.md
@@ -0,0 +1,31 @@
+Mine Sweeper Intro
+==================
+
+This is my _first_ javasscript **project**.
+Loads of ___learning___ occured during its construction and I soon learnt the _importance_ of **breaking** things down into ***smaller*** parts. 
+
+## Debugging
+* Takes time and patience.
+* Foundermentals such as data types are important to check.
+
+### Tips
+1. console.log/table everything
+
+## Links
+Here is a link to [click here](url) to play the game.
+
+## Images
+
+![alt text](url)
+
+## Code 
+
+You can Talk about variables or functions like `document.getElementById`
+
+For longer bits of code,use triple backticks
+```javascript
+const hey = function () {
+    console.log('Hey")
+}
+```
+
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 /*----- state variables -----*/
 const state = {
     currentTile: null, // tracks the selectored cell
-    minesInPlay: null, // number of mines without flags, a descending number
+    minesInPlay: 10, // number of mines without flags, a descending number
     flags: null, // flags left to use, a decending number
-    limbsLeft: null, // counts the number of limbs left( lives), is a decending mumber
+    limbsLeft: 4, // counts the number of limbs left( lives), is a decending mumber
 };
+
+minesNearBy = null;
 
 const field = [
     [0,0,0,0,0,0,0,0],
@@ -23,20 +25,23 @@ const field = [
 const rowLength = 8; // neesd to think about how to change these three when it comes to difficulyt selector
 const columnLength = 8;
 const numberOfMines = 10;
+//let row = null;
+//let column = null;
 
 let time = 10000000; // need to move to init?
 let startTime;    
 let elapsedTime;
   
 /*----- cached elements  -----*/
-//const goButton = document.getElementById('go'); 
+const goButton = document.getElementById('go'); 
 //const mineField = Array.from(document.querySelectorAll('#field > div'));
 
   
  /*----- event listeners -----*/
- //mineField.forEach(function (cell){
-   // cell.addEventListener('click', detectMine);
- //});
+ goButton.addEventListener('click', function(){
+    init();
+ })
+ 
 
  //document.querySelector('input[name="difficulty"]:checked')
   
@@ -52,23 +57,23 @@ const makeField = (size) => {
         makeCell.classList.add('cell');
         document.getElementById('field').appendChild(makeCell);
         makeCell.innerText = i;
-    if (i >= 0 && i <= 7) {
-        makeCell.setAttribute('id', 'r0' + 'c' + i);
-    }else if (i >= 8 && i <= 15 ) {
-        makeCell.setAttribute('id', 'r1' + 'c' + (i-8));
-    }else if (i >= 16 && i <= 23 ) {
-        makeCell.setAttribute('id', 'r2' + 'c' + (i-16));
-    }else if (i >= 24 && i <= 31 ) {
-        makeCell.setAttribute('id', 'r3' + 'c' + (i-24));
-    }else if (i >= 32 && i <= 39 ) {
-        makeCell.setAttribute('id', 'r4' + 'c' + (i-32));
-    }else if (i >= 39 && i <= 47 ) {
-        makeCell.setAttribute('id', 'r5' + 'c' + (i-40)); 
-    }else if (i >= 46 && i <= 55 ) {
-        makeCell.setAttribute('id', 'r6' + 'c' + (i-48));    
-    }else if (i >= 47 && i <= 63 ) {
-        makeCell.setAttribute('id', 'r7' + 'c' + (i-56));  
-    }; 
+        if (i >= 0 && i <= 7) {
+            makeCell.setAttribute('id', 'r0' + 'c' + i);
+        }else if (i >= 8 && i <= 15 ) {
+            makeCell.setAttribute('id', 'r1' + 'c' + (i-8));
+        }else if (i >= 16 && i <= 23 ) {
+            makeCell.setAttribute('id', 'r2' + 'c' + (i-16));
+        }else if (i >= 24 && i <= 31 ) {
+            makeCell.setAttribute('id', 'r3' + 'c' + (i-24));
+        }else if (i >= 32 && i <= 39 ) {
+            makeCell.setAttribute('id', 'r4' + 'c' + (i-32));
+        }else if (i >= 39 && i <= 47 ) {
+            makeCell.setAttribute('id', 'r5' + 'c' + (i-40)); 
+        }else if (i >= 46 && i <= 55 ) {
+            makeCell.setAttribute('id', 'r6' + 'c' + (i-48));    
+        }else if (i >= 47 && i <= 63 ) {
+            makeCell.setAttribute('id', 'r7' + 'c' + (i-56));  
+        }; 
   
 };
 attachEventListeners();
@@ -88,8 +93,8 @@ const init = () => {
     makeField(64);
     timeToStringConverter(time);
     startClock();
-    
-    
+    minesNearBy = 0;
+    randomiseMines(numberOfMines, rowLength, columnLength);
 };
 
 
@@ -123,16 +128,56 @@ const startClock = () => {
 };
   
 const detectMine = () => {
-console.log(typeof state.currentTile);
 const id = state.currentTile.id;
 // assuming rXcX
 // indexes: 0123
-const row = id[1]; // first X
-const column = id[3]; // second X
+let row = parseFloat(id[1]); // first X
+let column = parseFloat(id[3]); // second X
 
-console.log(row, column);
 
 if(field[row][column] === 'm') {
-console.log('bang');
+    console.log('mine');
+    landedOnMine();
+} else{
+    minesInProximity(row, column);
+
 }
+};
+
+const minesInProximity = (row, column) => {
+    document.getElementById(state.currentTile.id).style.backgroundColor = 'green';
+    console.log(typeof row, row, typeof column, column)
+   
+
+
+    for (let i = row - 1; i <= row + 1; i++) {
+        for (let c = column - 1; c <= column + 1; c++) {
+            if (i === row && c === column) {
+                continue; // Skip the current iteration
+            }
+            if (i >= 0 && i <= rowLength - 1 && c >= 0 && c <= columnLength - 1) {
+                if (field[i][c] === 'm') {
+                    minesNearBy++;
+                }
+            }
+        }
+    }
+};    
+
+
+
+
+    
+  
+
+    
+
+
+const landedOnMine = () => {
+    if(state.limbsLeft >= 1) {
+        state.limbsLeft -= 1;
+        alert(`Landed on mine - lost a limb. ${state.limbsLeft} limbs left.`)
+    }else if (state.limbsLeft === 0) {
+        alert('Game Over!'); 
+    }
 };
